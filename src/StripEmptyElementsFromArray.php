1bce9cf48bcc7058b6ec15f74fe5c24cc1f0ba39
Kevin Nauta <kevin@wefabric.nl>
2022-02-14 16:16:57 +0100

    Use isset() instead of !== 0

--- src/StripEmptyElementsFromArray.php
+++ src/StripEmptyElementsFromArray.php
@@ -19,7 +19,7 @@ class StripEmptyElementsFromArray
 
             //if we check: empty($value); and $value is an array that was emptied, then $value here still contains the pre-emptied values, whereas $data[$key] contains the now emptied array.
             if(empty($data[$key])) {
-                if(!$treatZeroAsFilled || $data[$key] !== 0) { //if value = (int) 0, (float) 0.0 or (string) "0", consider it NOT empty.
+                if(! $treatZeroAsFilled || ! isset($data[$key]) ) { //if value = (int) 0, (float) 0.0 or (string) "0", consider it NOT empty.
                     unset($data[$key]);
                 }
             }
