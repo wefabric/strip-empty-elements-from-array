c362dbbf6a96d3be6a57050412d992f3e249fa3b
Kevin Nauta <kevin@wefabric.nl>
2022-02-14 16:06:53 +0100

    Fix ?

--- src/StripEmptyElementsFromArray.php
+++ src/StripEmptyElementsFromArray.php
@@ -14,12 +14,12 @@ class StripEmptyElementsFromArray
     {
         foreach($data as $key => $value) {
             if (is_array($value)) {
-                $data[$key] = self::from($value);
+                $data[$key] = self::from($value, $treatZeroAsFilled);
             }
 
             //if we check: empty($value); and $value is an array that was emptied, then $value here still contains the pre-emptied values, whereas $data[$key] contains the now emptied array.
             if(empty($data[$key])) {
-                if($treatZeroAsFilled && $data[$key] !== 0) { //if value = (int) 0, (float) 0.0 or (string) "0", consider it NOT empty.
+                if(!$treatZeroAsFilled || $data[$key] !== 0) { //if value = (int) 0, (float) 0.0 or (string) "0", consider it NOT empty.
                     unset($data[$key]);
                 }
             }
